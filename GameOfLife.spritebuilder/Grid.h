42e7371c38e81740f7905bc85f4a3f04dda6efe6
klbxg <liweiwei0404@gmail.com>
2015-01-25 20:25:00 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,16 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Olivia Li on 1/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+// stores the current state of the creature
+
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Olivia Li on 1/25/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
